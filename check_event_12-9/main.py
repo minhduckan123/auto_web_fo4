03668ee05b4081bc6fefddb94b6c19c5d200760e
Stronger2 <93233277+minhduckan123@users.noreply.github.com>
2021-12-09 15:32:26 +0700

    Update main.py

--- check_event_12-9/main.py
+++ check_event_12-9/main.py
@@ -1,1 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
+url = "https://spin.fo4.garena.vn/"
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+driver.maximize_window()
+driver.get(url)
+
+with open("hello_text_01.txt", "r") as f:
+    read_file = [i.strip() for i in f.readlines()]
+
+for i in read_file:
+    username, password = i.split()
+    username, password = username.strip(), password.strip()
+
+    driver.find_element_by_xpath("//a[@href = '/user/login']").click()
+
+    driver.find_element_by_name("username").send_keys(username)
+    driver.find_element_by_name("password").send_keys(password)
+    time.sleep(1)
+
+    # Dang nhap
+    driver.find_element_by_id("confirm-btn").click()
+
+    # Chon muc nhiem vu
+    element = WebDriverWait(driver, 1800).until(EC.presence_of_element_located(
+        (By.XPATH, "//a[@class = 'animate__animated animate__tada animate__infinite']")))
+    element.click()
+    time.sleep(2)
+    try:
+        # Cap nhat nhiem vu
+        driver.find_element_by_class_name("mission__action").click()
+        driver.find_element_by_class_name("close").click()
+    except:
+        try:
+            # Da cap nhat nhiem vu
+            driver.find_element_by_class_name("close").click()
+        except:
+            # Nhiem vu chua hoan thanh
+            element_close = WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located((By.XPATH, "//button[@type = 'button']")))
+            element_close.click()
+            driver.find_element_by_class_name("close").click()
+
+    element_close = WebDriverWait(driver, 1800).until(
+        EC.presence_of_element_located((By.XPATH, "//a[@href='/user/logout']")))
+    element_close.click()
+
+    print("Logged in successfully")
+print("Logout successfully")
+print("\n-----Everything donewell!!-----\n")
